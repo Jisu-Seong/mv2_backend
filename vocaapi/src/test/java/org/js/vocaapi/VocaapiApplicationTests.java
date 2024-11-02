77cf8de5f5487d88bbc937dab95acf9ad41ad97b
Jisu-Seong <sjangela0615@gmail.com>
2024-11-02 15:54:58 +0900

    upload

---
+++ vocaapi/src/test/java/org/js/vocaapi/VocaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.js.vocaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
